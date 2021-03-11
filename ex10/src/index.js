a34b26ba05f00d3151b495ecdc32164d84271936
basslines <admir.osmanovic@gmail.com>
2021-03-11 18:45:45 +0100

    dodao index.js fajl

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    var myFirstName = "Admir";
+    var myLastName = "Osmanovic";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
